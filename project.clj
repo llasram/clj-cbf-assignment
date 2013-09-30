649107f9ace9fc8a0a0c8a71865c1ceb99c2986f
Donald Clark Jackson <dcj@clark-communications.com>
2013-09-29 21:56:04 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject clj-cbf-assignment "0.1.0-SNAPSHOT"
+  :description "Clojure version of"
+  :url "https://gist.github.com/llasram/6472144"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :main ^:skip-aot cbf.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.grouplens.lenskit/lenskit-knn "2.0.2"]
+                 [org.grouplens.lenskit/lenskit-core "2.0.2"]
+                 [org.grouplens.lenskit/lenskit-api "2.0.2"]
+                 [org.grouplens.lenskit/lenskit-data-structures "2.0.2"]
+                 [org.slf4j/slf4j-api "1.7.5"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
+                 [log4j "1.2.17"]])
---
+++ src/clojure/cbf/core.clj
@@ -0,0 +1,68 @@
+(ns cbf.core
+  (:require [clojure.java.io :as io])
+
+  (:import [org.grouplens.lenskit
+            ItemRecommender ItemScorer Recommender RecommenderBuildException]
+           [org.grouplens.lenskit.core
+            LenskitConfiguration LenskitRecommender]
+           [org.grouplens.lenskit.data.dao
+            EventDAO ItemDAO UserDAO]
+           [org.grouplens.lenskit.scored ScoredId]
+           [cbf
+            TFIDFItemScorer TFIDFModel TFIDFModelBuilder]
+           [cbf.dao 
+            CSVItemTagDAO ItemTagDAO ItemTitleDAO MOOCItemDAO MOOCRatingDAO MOOCUserDAO
+            RatingFile TagFile TitleFile UserFile UserNameDAO]))
+
+(defn- configure-recommender
+  []
+  (doto (LenskitConfiguration.)
+    ;; configure the rating data source
+    (-> (.bind EventDAO) 
+        (.to MOOCRatingDAO))
+    (-> (.set RatingFile)
+        (.to (io/file "data/ratings.csv")))
+    ;; use custom item and user DAOs
+    ;; specify item DAO implementation with tags
+    (-> (.bind ItemDAO)
+        (.to CSVItemTagDAO))
+    ;; specify tag file
+    (-> (.set TagFile)
+        (.to (io/file "data/movie-tags.csv")))
+    ;; and title file
+    (-> (.set TitleFile)
+        (.to (io/file "data/movie-titles.csv")))
+    ;; our user DAO can look up by user name
+    (-> (.bind UserDAO)
+        (.to MOOCUserDAO))
+    (-> (.set UserFile)
+        (.to (io/file "data/users.csv")))
+    ;; use the TF-IDF scorer you will implement to score items
+    (-> (.bind ItemScorer)
+        (.to TFIDFItemScorer))))
+                     
+(defn run
+  [users]
+  (let [config (configure-recommender)
+        rec    (LenskitRecommender/build config)
+        irec   (.getItemRecommender rec)]
+    (try
+      (doseq [user users
+              :let [recs (.recommend irec user 5)]]
+        (when (empty? recs)
+          (println "no recommendations for user:" user "do they exist?"))
+        (println "recommendations for user:" user)
+        (doseq [id recs]
+          (->> id .getId (format "\t%d\n") print)))
+      (catch UnsupportedOperationException e
+        (when (= (.getMessage e)
+                 "stub implementation")
+          (println "Congratulations, the stub builds and runs!"))))))
+      
+
+(defn -main
+  [& args]
+  (run (mapv #(Long/parseLong %)
+             args)))
+
+
